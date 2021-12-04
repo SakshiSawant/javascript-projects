6c556f84fbb6c31dbeb594432d1731ef802e28ab
sakshi192000 <sakshisawants4100@gmail.com>
2021-12-04 18:37:18 +0530

    refactor

--- countdownTimer/index.js
+++ countdownTimer/index.js
@@ -1,18 +1,25 @@
 let countDate = document.getElementById("countdown_timer_date")
 let countHours = document.getElementById("countdown_timer_hours")
 let countMinutes = document.getElementById("countdown_timer_minutes")
+let countSeconds = document.getElementById("countdown_timer_seconds")
 
+function countDown() {
+    let countdown = 25
+    let currentDate = new Date()
+    let currentDay = currentDate.getDate()
+    let currentHour = currentDate.getHours()
+    let currentMinute = currentDate.getMinutes()
+    let currentSecond = currentDate.getSeconds()
 
-let countdown = 25
-let currentDate = new Date()
-let currentDay = currentDate.getDate()
-let currentHour = currentDate.getHours()
-let currentMinute = currentDate.getMinutes()
+    countDate.textContent = countdown - currentDay
+    countHours.textContent = 24 - currentHour
+    countMinutes.textContent = 60 - currentMinute
+    countSeconds.textContent = 60 - currentSecond
+}
 
-countDate.textContent = countdown - currentDay
-countHours.textContent = 24 - currentHour
-countMinutes.textContent = 60 - currentMinute
+countDown()
 
-setTimeout(function() {
-  location.reload();
-}, 30000);
+setTimeout(function () {
+    location.reload()
+},3000)
+
